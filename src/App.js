0a96331d7338b1b2e19f9c09068732ed192acecd
Oliver Graml <o.graml@gmx.de>
2021-05-11 23:00:39 +0200

    implemented colorize-number

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
 import './App.css';
 import {Button, addOne} from './Button';
-import { useState } from 'react'; 
+import {useState} from 'react';
 
 function App() {
   const [number, setNumber] = useState(0);
 
- /*  randomHoe forever ❤️ */
+  function ColorizeNumber() {
+    return <p style={{color: `hsl(${number}, 100%, 50%)`}}> {number}</p>;
+  }
+  /*  randomHoe forever ❤️ */
 
   return (
     <div className="App">
       <h1>Random Calculator</h1>
       <h3>Created with React</h3>
-      <p style={{color: `hsl(${number}, 100%, 50%)`}}>{number}</p>
+      <ColorizeNumber />
       <section>
-      <button className="button" onClick={() => setNumber(Math.floor((-Math.random()*360) + number))}>-random</button>
-      <button className="button" onClick={() => setNumber(number - 10)}>-10</button>
-      <button className="button" onClick={() => setNumber(number - 1)}>-1</button>
-      <button className="button" onClick={() => setNumber(number + 1)}>+1</button>
-      <button className="button" onClick={() => setNumber(number + 10)}>+10</button>
-      <button className="button" onClick={() => setNumber(Math.floor(Math.random()*360 + number))}>+random</button>
+        <button
+          className="button"
+          onClick={() => setNumber(Math.floor(-Math.random() * 360 + number))}
+        >
+          -random
+        </button>
+        <button className="button" onClick={() => setNumber(number - 10)}>
+          -10
+        </button>
+        <button className="button" onClick={() => setNumber(number - 1)}>
+          -1
+        </button>
+        <button className="button" onClick={() => setNumber(number + 1)}>
+          +1
+        </button>
+        <button className="button" onClick={() => setNumber(number + 10)}>
+          +10
+        </button>
+        <button
+          className="button"
+          onClick={() => setNumber(Math.floor(Math.random() * 360 + number))}
+        >
+          +random
+        </button>
       </section>
-      <button className="button" onClick={() => setNumber(0)}>Reset</button>
+      <button className="button" onClick={() => setNumber(0)}>
+        Reset
+      </button>
     </div>
   );
 }
-
 
 export default App;
